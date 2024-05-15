ce1258f037d272a2118ebe992f814506a38dce04
txw <xw_tong@163.com>
2024-05-15 11:05:20 +0800

    update6

--- Ae.Product.Service/Ae.Product.Service/Startup.cs
+++ Ae.Product.Service/Ae.Product.Service/Startup.cs
@@ -27,6 +27,9 @@ using Ae.Product.Service.Core.Interfaces;
 using Ae.Product.Service.Validators;
 using Ae.Product.Service.Core.Extension;
 using Ae.Product.Service.Filters;
+using Ae.Product.Service.Common.Format;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace Ae.Product.Service
 {
@@ -43,12 +46,15 @@ namespace Ae.Product.Service
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddJsonOptions(options => { options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss"; })
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddFluentValidation(o =>
-                {
-                    o.RegisterValidatorsFromAssemblyContaining<AddBrandRequestValidator>();
-                });
+            //services.AddMvc()
+            //    .AddJsonOptions(options => { options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss"; })
+            //    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddFluentValidation(o =>
+            //    {
+            //        o.RegisterValidatorsFromAssemblyContaining<AddBrandRequestValidator>();
+            //    });
+            services.AddControllers()
+            .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new DatetimeJsonConverter("yyyy-MM-dd HH:mm:ss")); });
+
             //配置跨域处理
             services.AddCors(options =>
             {
@@ -56,8 +62,8 @@ namespace Ae.Product.Service
                 {
                     builder.AllowAnyOrigin() //允许任何来源的主机访问
                         .AllowAnyMethod()
-                        .AllowAnyHeader()
-                        .AllowCredentials(); //指定处理cookie
+                        .AllowAnyHeader();
+                        //.AllowCredentials(); //指定处理cookie
                 });
             });
             // override modelstate
@@ -111,9 +117,10 @@ namespace Ae.Product.Service
             services.AddMvcCore(option => { option.Filters.Add(new FilterCorrelationIdAttribute(string.Empty)); });
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (!env.IsProduction())
+            app.UseRouting();
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
 
@@ -144,11 +151,22 @@ namespace Ae.Product.Service
 
             app.UseHttpsRedirection();
             app.UseCors("AllowCors");
-            app.UseMvc(routes =>
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        "default",
+            //        "{controller=Home}/{action=Index}");
+            //});
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}");
+                // 设置默认路由
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello, World!");
+                });
+
+                // 配置控制器路由
+                endpoints.MapControllers();
             });
         }
     }
